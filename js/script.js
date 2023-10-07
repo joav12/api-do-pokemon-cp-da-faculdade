6063225c9d1f7c21f23c55ff0dce9b83bcd44365
João Victor Rosinhole <61693806+joav12@users.noreply.github.com>
2023-10-06 21:38:26 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//Data Model
+class Pokemon{
+    constructor(id, nome, img, tipo1){
+        this.nome = nome;
+        this.id = id;
+        this.img = img;
+        this.tipo1 = tipo1;
+    }
+
+    static fromJson(json){
+        return new Pokemon(json.id, json.name,json.sprites.front_default, json.types[0].type.name);
+    }
+}
+
+async function fetchData(){
+    try{
+        //Realiza a requisição para a API
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+
+        if(!response.ok){
+            throw new Error('Erro na rede');
+        }
+
+        // Converte a API em JSON
+        const data = await response.json();
+
+        //Exibe os dados da API
+        // console.log(data);
+
+        const poke1 = Pokemon.fromJson(data);
+
+        return poke1;
+
+    } catch(error){
+        //Exime o erro no console
+        console.log('Erro:', error);
+    }
+}
+
+let poke_nome = document.getElementById('poke_nome');
+let poke_id = document.getElementById('poke_id');
+let poke_img = document.getElementById('poke_img');
+let poke_primeiro_tipo = document.getElementById('poke_primeiro_tipo');
+
+//Aqui dentro conseguimos utilizar normalmente a api e fazermos oq quisermos com os dados dela
+fetchData().then(poke1 =>{
+    poke_nome.innerHTML = `O nome do pokemon é: ${poke1.nome}`;
+
+    poke_id.innerHTML = `Seu id é: ${poke1.id}`;
+
+    poke_img.src = poke1.img;
+
+    poke_primeiro_tipo.innerHTML = `E seu primeiro tipo (em inglês) é: ${poke1.tipo1}`
+})
